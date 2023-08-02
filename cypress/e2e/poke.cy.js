4b6e7f9d3f5d32bff2071bf3c6c634bd1a7ec542
scott-hall7 <scotthall179@gmail.com>
2023-08-02 09:40:49 -0500

    adding specific pokemon test

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('pokemon can be navigated to', function() {
+    cy.visit('http://localhost:5001')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('chlorophyll')
+  })
 })
